df4ce2f4a5860ba7eb1fdfbd6a79bef1edcd0c5c
Martin Pitt <martinpitt@gnome.org>
2012-05-22 11:21:26 +0200

    UbuntuDrivers: Add some documentation and copyright headers

--- UbuntuDrivers/packagekit_plugin.py
+++ UbuntuDrivers/packagekit_plugin.py
@@ -1,3 +1,25 @@
+'''PackageKit plugin for WhatProvides() call for type MODALIAS
+
+ With this you can ask PackageKit about "which package do I need to install to
+ provide a driver for the device pci:v00001234...?", for example:
+
+ $ pkcon what-provides "pci:v000010DEd000007E3sv00sd00bc03sc00i00"
+ Available     nvidia-current-295.49-0ubuntu1.amd64        NVIDIA binary Xorg driver, kernel module and VDPAU library
+ Available     nvidia-current-updates-295.49-0ubuntu1.amd64    NVIDIA binary Xorg driver, kernel module and VDPAU library
+'''
+
+# Note that this does not work with PackageKit's "aptcc" backend as that does
+# not support plugins. You need to use PackageKit's "apt" backend or
+# python-aptdaemon.pkcompat on Debian/Ubuntu (preferred).
+#
+# (C) 2012 Canonical Ltd.
+# Author: Martin Pitt <martin.pitt@ubuntu.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
 import logging
 import fnmatch
 import re
--- tests/ubuntu_drivers.py
+++ tests/ubuntu_drivers.py
@@ -1,3 +1,11 @@
+# (C) 2012 Canonical Ltd.
+# Author: Martin Pitt <martin.pitt@ubuntu.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
 import os
 import time
 import unittest
