5ea0d36c3d5d6db7cdc7da5575f8bfb1719c730e
Martin Pitt <martinpitt@gnome.org>
2012-09-04 08:48:42 +0200

    setup.py: Only install the upstart job on platforms where we build hybrid-detect. (LP: #1045193)

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ extra_data = []
 if '86' in os.uname()[4]:
     subprocess.check_call(["make", "-C", "share/hybrid", "all"])
     extra_data.append(("/usr/bin/", ["share/hybrid/hybrid-detect"]))
+    extra_data.append(("/etc/init/", glob.glob("share/hybrid/hybrid-gfx.conf")))
 
 # Make the nvidia-installer hooks executable
 for x in glob.glob("nvidia-installer-hooks/*"):
@@ -27,7 +28,7 @@ setup(
     packages=["NvidiaDetector", "Quirks", "UbuntuDrivers"],
     data_files=[("/usr/share/ubuntu-drivers-common/", ["share/obsolete", "share/fake-devices-wrapper"]),
                 ("/var/lib/ubuntu-drivers-common/", []),
-                ("/etc/init/", glob.glob("share/hybrid/hybrid-gfx.conf")),
+                ("/etc/", []),
                 ("/usr/share/ubuntu-drivers-common/quirks", glob.glob("quirks/*")),
                 ("/usr/share/ubuntu-drivers-common/detect", glob.glob("detect-plugins/*")),
                 ("/usr/share/doc/ubuntu-drivers-common", ['README']),
