53581e69e9282c2d2b469f6d7f218f80029d88c3
Tim Lunn <tim@feathertop.org>
2014-05-28 09:02:43 +1000

    enable_prime on gdm as well

--- share/hybrid/gpu-manager.c
+++ share/hybrid/gpu-manager.c
@@ -551,6 +551,13 @@ static int is_lightdm_default() {
             "lightdm"));
 }
 
+/* Check if gdm is the default login manager */
+static int is_gdm_default() {
+
+    return (find_string_in_file("/etc/X11/default-display-manager",
+            "gdm"));
+}
+
 
 static void detect_available_alternatives(struct alternatives *info, char *pattern) {
     if (strstr(pattern, "mesa")) {
@@ -2100,7 +2107,7 @@ static int enable_prime(const char *prime_settings,
     int prime_action_on = 0;
 
     /* We only support Lightdm at this time */
-    if (!is_lightdm_default()) {
+    if (!(is_lightdm_default() || is_gdm_default())) {
         fprintf(log_handle, "Lightdm is not the default display "
                             "manager. Nothing to do\n");
         return 0;
