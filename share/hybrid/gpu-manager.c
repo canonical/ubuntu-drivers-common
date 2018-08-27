canonical/ubuntu-drivers-common

commit 6770360ed3324358d9069c7468a3af80219157ef

Check also /lib/modprobe.d when looking for blacklisted modules

share/hybrid/gpu-manager.c
@@ -381,6 +381,15 @@ static bool is_module_blacklisted(const char* module) {
                  module, modprobe_d_path);
 
         match = get_output(command, NULL, NULL);
+
+        if (!match) {
+            free(match);
+            snprintf(command, sizeof(command),
+                 "grep -G \"^blacklist.*%s[[:space:]]*$\" %s/*.conf",
+                 module, "/lib/modprobe.d");
+
+            match = get_output(command, NULL, NULL);
+        }
     }
 
     if (!match)

commit 2f7a726af3e4b7fa2022824bd72f5c0fad6ec51f

gpu-manager.c: Fix potential issue introduced in 6770360ed3324358d9069c7468a3af80219157ef

share/hybrid/gpu-manager.c
@@ -382,7 +382,6 @@ static bool is_module_blacklisted(const char* module) {
         match = get_output(command, NULL, NULL);
 
         if (!match) {
-            free(match);
             snprintf(command, sizeof(command),
                  "grep -G \"^blacklist.*%s[[:space:]]*$\" %s/*.conf",
                  module, "/lib/modprobe.d");