25d1bd8a2e91a92689c07a313089a32fb5d8fbe3
Alberto Milone <alberto.milone@canonical.com>
2020-11-09 17:09:39 +0100

    Prefer LTSB releases over NFB ones for non gpgpu cases too
    This makes ubuntu-drivers more conservative in its choices.
    
    This also adds a test case for the new behaviour.

--- UbuntuDrivers/detect.py
+++ UbuntuDrivers/detect.py
@@ -920,11 +920,18 @@ def _pkg_support_from_cache(x):
 def _cmp_gfx_alternatives(x, y):
     '''Compare two graphics driver names in terms of preference. (desktop)
 
-    NFB (New Feature Branch) or the latest is what we recommend on the desktop.
     -server always sorts after non-server.
     LTSB (Long Term Support Branch) always sorts before NFB (New Feature Branch).
     Legacy always sorts before Beta.
     '''
+    if x.endswith('-server') and not y.endswith('-server'):
+        return -1
+    if not x.endswith('-server') and y.endswith('-server'):
+        return 1
+    if _pkg_support_from_cache(x) == 'LTSB' and _pkg_support_from_cache(y) != 'LTSB':
+        return 1
+    if _pkg_support_from_cache(x) != 'LTSB' and _pkg_support_from_cache(y) == 'LTSB':
+        return -1
     if _pkg_support_from_cache(x) == 'Legacy' and _pkg_support_from_cache(y) != 'Legacy':
         return -1
     if _pkg_support_from_cache(x) != 'Legacy' and _pkg_support_from_cache(y) == 'Legacy':
@@ -932,10 +939,6 @@ def _cmp_gfx_alternatives(x, y):
     if _pkg_support_from_cache(x) == 'Beta' and _pkg_support_from_cache(y) != 'Beta':
         return -1
     if _pkg_support_from_cache(x) != 'Beta' and _pkg_support_from_cache(y) == 'Beta':
-        return 1
-    if x.endswith('-server') and not y.endswith('-server'):
-        return -1
-    if not x.endswith('-server') and y.endswith('-server'):
         return 1
     if x < y:
         return -1
